236ddb5a6b1554c04bfb282958b5164f66da45db
Katelin Zichittella <katelinz2@frontier.com>
2018-05-14 14:55:58 -0400

    Disabled RelishAutonomous

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RelishAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RelishAutonomous.java
@@ -1,12 +1,14 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 
 /**
  * Created by Fiona Beer on 4/25/2018.
  */
 
 @Autonomous
+@Disabled
 
 public class RelishAutonomous extends RelishAutonomousHeader {
 
